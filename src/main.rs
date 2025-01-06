761bcece602c004b9c9998b370bac4aa8ba3af39
Thomas Castiglione <castiglione@mac.com>
2025-01-06 09:12:32 +0800

    WIP

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use text::{newline, whitespace};
 enum Block {
     Empty,
     Graf(Vec<String>),
+    Heading(String),
 }
 
 #[derive(Debug)]
@@ -18,14 +19,7 @@ fn main() -> Result<()> {
     stdin().lock().read_to_string(&mut input)?;
 
     match parser().parse(input.clone()) {
-        Ok(ast) => {
-            for block in ast.0.into_iter() {
-                match block {
-                    Block::Empty => println!("<br>"),
-                    Block::Graf(lines) => println!("<p>{0}</p>", lines.join(" ").trim_end()),
-                }
-            }
-        }
+        Ok(ast) => println!("{ast:?}"),
         Err(errs) => errs.into_iter().for_each(|e| {
             Report::build(ReportKind::Error, e.span())
                 .with_message(e.to_string())
@@ -48,12 +42,19 @@ fn parser() -> impl Parser<char, Doc, Error = Simple<char>> {
 
     let empty_block = newline().then(whitespace()).to(Block::Empty);
 
+    //let marker = just("#")
+
     let graf_block = content
         .separated_by(newline())
         .at_least(1)
         .map(|lines| Block::Graf(lines));
 
-    choice((graf_block, empty_block))
+    let heading_block = just('#')
+        .then(whitespace())
+        .ignore_then(content)
+        .map(|line| Block::Heading(line));
+
+    choice((heading_block, graf_block, empty_block))
         .repeated()
         .map(|blocks| Doc(blocks))
         .then_ignore(end())
